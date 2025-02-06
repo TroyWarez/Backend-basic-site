b0fa88ac2fa9dc3be26aad917f1bdd356745c21a
Troy <TroyWarez@troysdomain.com>
2025-02-05 21:49:07 -0700

    basic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
+import "dotenv/config";
+import bodyParser from "body-parser";
+import express, { Express } from "express";
+import mongoose from "mongoose";
 import cors from "cors";
+
+const app: Express = express();
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS_CORS,
+  })
+);
