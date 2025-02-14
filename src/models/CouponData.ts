0c736c5f6966e014301cf3b7e68014369abc1a2f
Troy <TroyWarez@troysdomain.com>
2025-02-14 14:41:02 -0700

    removed string

--- src/models/CouponData.ts
+++ src/models/CouponData.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const couponDataSchema = new mongoose.Schema({
-  _id: { type: String, required: false },
+  _id: { type: mongoose.Types.ObjectId, required: false },
   coupon: { type: String, required: true },
   discount: { type: Number, required: false },
   expires: { type: Date, required: false },
--- src/routes/couponCodesRoutes.ts
+++ src/routes/couponCodesRoutes.ts
@@ -1,25 +1,39 @@
 import express, { Request, Response } from "express";
 import HttpError from "../models/HttpError";
 import Coupon from "../models/CouponData";
+import mongoose from "mongoose";
 import { handleRequestError } from "../utils";
-import { INVALID_COUPON } from "../constants/errorMessages";
 const couponCodesRouter = express.Router();
-
-couponCodesRouter.use(express.json());
 
 couponCodesRouter.get("/:coupon", async (_req: Request, res: Response) => {
     try {
       const coupons = await Coupon.find({coupon: _req.params.coupon});
-      if( coupons.length > 0 && (coupons[0]?.expires as Date).getFullYear() < new Date().getFullYear()) {
-        res.status(200).send([{discountPercent: coupons[0]?.discount}]);
+      res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS_CORS);
+      if( coupons.length > 0 ) {
+        if((coupons[0]?.expires as Date).getTime() >= new Date().getTime())
+        {
+          res.status(200).send([{discountPercent: coupons[0]?.discount}]);
+        }
+        else
+        {
+          coupons.forEach(coupon => {
+            try {
+            coupon.deleteOne({ _id: coupon.id });
+            }
+            catch(e) {
+              console.log(e);
+            }
+          })
+          throw new Error('The coupon \'' + _req.params.coupon + '\' is expired.');
+        }
       }
       else {
         throw new Error('The coupon ' + _req.params.coupon + ' was not found.');
       }
-    } catch (error) {
+    } catch (error: any) {
       const httpError: HttpError = {
         httpCode: 400,
-        message: INVALID_COUPON,
+        message: error.message,
         error,
       };
       return handleRequestError(res, httpError);
