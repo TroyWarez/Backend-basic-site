a9d926e0d78556b6711f5348d6ac26876fb6a502
Troy <TroyWarez@troysdomain.com>
2025-05-20 10:11:04 -0700

    html server

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,28 @@ import userRouter from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
 import cartRouter from "./routes/cartRoutes";
 import addressRoutes from "./routes/addressRoutes";
+import serveStatic from "serve-static";
 const app: Express = express();
+const port = 443;
 
+const PORT = 3000;
+
+
+// Mount the middleware at "/static" to serve static content only when their request path is prefixed with "/static".
+
+    // GET /static/style.css etc.
+app.use('/', express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public/assets/'));
+app.use('/checkout', express.static(__dirname + '/public'));
+app.use('/checkout/cart', express.static(__dirname + '/public'));
+app.use('/signup', express.static(__dirname + '/public'));
+app.use('/customer/account/login', express.static(__dirname + '/public'));
+app.use('/legal/privacy/privacy-policy', express.static(__dirname + '/public'));
+app.use('/terms-and-conditions', express.static(__dirname + '/public'));
+app.use('/legal', express.static(__dirname + '/public'));
+app.use('/order-status', express.static(__dirname + '/public'));
+
+app.listen(port, () => console.log(`Server listening on port: ${port} ${__dirname}`));
 app.use(bodyParser.json());
 
 app.use(
@@ -31,16 +51,13 @@ app.use("/api/cart", cartRouter);
 
 app.use("/api/addresses", addressRoutes);
 
-
-
-
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING!)
   .then(() => {
     console.log(`Successfully connected to the db.`);
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Listening on http://localhost:${PORT}`);
+    app.listen(3000, () => {
+      console.log(`Listening on http://localhost:${3000}`);
     });
   })
   .catch((e) =>
